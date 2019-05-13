76b6b14f73b347d94453cbf3d771ac4e51d6c502
Гелцен Савындай <5842_sgy@keva.int>
2019-05-13 12:49:47 +0700

    Added SDK project.

---
+++ SDK/BaseFigure.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+
+namespace SDK
+{
+    /// <summary>
+    /// Базовая сущность для фигуры
+    /// </summary>
+    [JsonObject(MemberSerialization.Fields)]
+    public abstract class BaseFigure
+    {
+        /// <summary>
+        /// Параметры линии
+        /// </summary>
+        protected LineProperties _lineProperties;
+
+        /// <summary>
+        /// Точки, по которым строится фигура
+        /// </summary>
+        protected Points _points;
+
+        /// <summary>
+        /// Свойства для параметров линии
+        /// </summary>
+        public LineProperties LineProperties
+        {
+            get
+            {
+                return _lineProperties;
+            }
+            set
+            {
+                _lineProperties = value;
+            }
+        }
+
+        /// <summary>
+        /// Свойства для точек
+        /// </summary>
+        public Points Points
+        {
+            get
+            {
+                return _points;
+            }
+            set
+            {
+                _points = value;
+            }
+        }
+    }
+}
---
+++ SDK/FillableFigure.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace SDK
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Базовый класс для фигур с заполнением
+    /// </summary>
+    [JsonObject(MemberSerialization.Fields)]
+    public abstract class FillableFigure: BaseFigure
+    {
+        /// <summary>
+        /// Цвет заполнения
+        /// </summary>
+        protected FillProperty _fillProperty;
+
+        /// <summary>
+        /// Свойство для цвета заполнения
+        /// </summary>
+        public FillProperty FillProperty
+        {
+            get
+            {
+                return _fillProperty;
+            }
+            set
+            {
+                _fillProperty = value;
+            }
+        }
+    }
+}
