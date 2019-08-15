01b263be5dbbcca4b2ee89a0e88802d6a2e330fc
Гелцен Савындай <5842_sgy@keva.int>
2019-08-16 00:02:02 +0700

    Added IDrawerFacade interface

---
+++ VectorEditorCore/Drawers/IDrawerFacade.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+using SDK;
+
+namespace VectorEditorCore.Drawers
+{
+    /// <summary>
+    /// Интерфейс, используемый
+    /// для рисования фигур и маркеров
+    /// </summary>
+    interface IDrawerFacade
+    {
+        /// <summary>
+        /// Нарисовать фигуру
+        /// </summary>
+        /// <param name="figure">Фигура</param>
+        /// <param name="graphics">Поврхность рисования</param>
+        void DrawFigure(BaseFigure figure, Graphics graphics);
+
+        /// <summary>
+        /// Нарисовать маркеры
+        /// </summary>
+        /// <param name="figure">Выделенная фигура</param>
+        /// <param name="graphics">Поверхность рисования</param>
+        void DrawMarkers(BaseFigure figure, Graphics graphics);
+    }
+}
